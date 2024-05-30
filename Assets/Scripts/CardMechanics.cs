b7fc38e5b040be330c68edaff36a2d69f932151c
Vreth-r <michaellatka49@gmail.com>
2024-05-30 12:42:04 -0400

    Changed Card prefab rendering

---
+++ Assets/Scripts/CardMechanics.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    CardMechanics Script:
+    - Handles card mechanics and effects as well as card user interaction
+*/
+
+public class CardMechanics : MonoBehaviour
+{
+    
+}
